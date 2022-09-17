e3a58ab6b48e8a4b6aee9b16b749717e6f20ba34
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-09-17 15:29:00 +0530

    PUT data into firebase: successsful

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Layout from './components/Layout/Layout';
 import Card from './components/UI/Card';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react';
 import Spotify from './components/Spotify/Spotify'
 
 function App() {
     const showCart = useSelector(state => state.showCart);
     const cart = useSelector(state => state.cart);
 
+    useEffect(() => {
+        const sendCartData = async () => {
+            const response = await fetch('https://redux-cart-9ba07-default-rtdb.firebaseio.com/cart.json',
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(cart),
+                }
+            )
+            if(!response.ok){
+                throw new Error('sending data failed');
+            }
+            const responseData = await response.json();
+        }
+        sendCartData();
+    }, [cart])
     return (
         <Layout>
             {showCart.show && <Cart />}
